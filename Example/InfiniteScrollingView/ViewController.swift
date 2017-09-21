300fbf6e46ccecd8b0e1a65d4887cd5b28ac1f2b
jerry <445242970@qq.com>
2017-09-22 00:43:50 +0800

    update for swift4

--- Example/InfiniteScrollingView/ViewController.swift
+++ Example/InfiniteScrollingView/ViewController.swift
@@ -7,18 +7,91 @@
 //
 
 import UIKit
+import InfiniteScrollingView
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var testView: InfiniteScrollingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        testView.aDataSource = self
+        testView.aDelegate = self
+        //        testView.allowInfiniteScrolling = false
+        
+        testView.didScrollAt = {index in
+            print("didScrollToIndex = \(index)")
+        }
+        
+        //        testView.registerReusableCell(TestCollectionViewCell.self)
+//        testView.regis
+//        testView.registerClass(cellClass: TestCollectionViewCell.self, forCellWithReuseIdentifier: TestCollectionViewCell.reuseIdentifier)
+        //        testView.scrollDirection = .Vertical
+        testView.isPagingEnabled = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func clickButton(sender: AnyObject) {
+        
+        //        if testView.scrollDirection == .Vertical {
+        //            testView.scrollDirection = .Horizontal
+        //        }else {
+        //            testView.scrollDirection = .Vertical
+        //        }
+        
+        //if testView.allowInfiniteScrolling == true {
+        //    testView.allowInfiniteScrolling = false
+        //}else {
+        //    testView.allowInfiniteScrolling = true
+        //}
+        
+        testView.scrollToNextPage(animated: true)
+    }
+}
+
+
+extension ViewController: InfiniteScrollingViewDataSource {
+    
+    func numberOfItems(in infiniteScrollingView: InfiniteScrollingView) -> Int {
+        return 3
+    }
+    
+    func infiniteScrollingView(_ infiniteScrollingView: InfiniteScrollingView, cellForItemAt index: Int) -> UICollectionViewCell {
+        
+        let cell = infiniteScrollingView.dequeueReusableCell(withReuseIdentifier: "Cell", for: index)
+        if index == 0 {
+            cell.backgroundColor = UIColor.red
+        }else if index == 1 {
+            cell.backgroundColor = UIColor.green
+        }else {
+            cell.backgroundColor = UIColor.blue
+        }
+        return cell
+    }
+}
+
+extension ViewController: InfiniteScrollingViewDelegate {
+    
+//    func infiniteScrollingView(_ infiniteScrollingView: InfiniteScrollingView, didSelectItemAt index: Int) {
+//        print("didSelectItemAtIndex: \(index)")
+//    }
+//    
+//    func itemLength(in infiniteScrollingView: InfiniteScrollingView) -> CGFloat {
+//        return 100
+//    }
+//    
+//    func itemSpacing(in infiniteScrollingView: InfiniteScrollingView) -> CGFloat {
+//        return 20
+//    }
+//    
+//    func padding(in infiniteScrollingView: InfiniteScrollingView) -> CGFloat {
+//        return 30
+//    }
 
 }
 
--- InfiniteScrollingView.podspec
+++ InfiniteScrollingView.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'InfiniteScrollingView'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of InfiniteScrollingView.'
+  s.version          = '0.4.0'
+  s.summary          = 'InfiniteScrollingView is for infinte scrolling.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,14 +18,14 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+InfiniteScrollingView is for infinte scrolling, especilly for banner.
                        DESC
 
-  s.homepage         = 'https://github.com/<GITHUB_USERNAME>/InfiniteScrollingView'
+  s.homepage         = 'https://github.com/Gaea-iOS/InfiniteScrollingView'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'wangxiaotao' => '445242970@qq.com' }
-  s.source           = { :git => 'https://github.com/<GITHUB_USERNAME>/InfiniteScrollingView.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/Gaea-iOS/InfiniteScrollingView.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
